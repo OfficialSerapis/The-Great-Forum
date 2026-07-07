d0aeea7280ee2d557a2f28511eafadd35264a423
OfficialSerapis <143079761+OfficialSerapis@users.noreply.github.com>
2026-07-07 12:55:16 +0100

    Add files via upload

--- map.js
+++ map.js
@@ -1,6 +1,8 @@
-var concatMap = require('../');
-var xs = [ 1, 2, 3, 4, 5, 6 ];
-var ys = concatMap(xs, function (x) {
-    return x % 2 ? [ x - 0.1, x, x + 0.1 ] : [];
+'use strict';
+
+var Type = require('../type');
+
+module.exports = new Type('tag:yaml.org,2002:map', {
+  kind: 'mapping',
+  construct: function (data) { return data !== null ? data : {}; }
 });
-console.dir(ys);
